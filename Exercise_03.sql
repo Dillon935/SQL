bc55898be6d8cf5c9ac93a4b428b3bb05063d8a6
Dillon935 <123113882+Dillon935@users.noreply.github.com>
2023-03-26 13:36:27 -0400

    Update Exercise_03.sql

--- Exercise_03.sql
+++ Exercise_03.sql
@@ -1,8 +1,12 @@
 SELECT
 	student.first_name as student_first_name,
-    student.last_name as student_last_name
+    student.last_name as student_last_name,
+    instructor.first_name as instructor_first_name,
+    instructor.last_name as instructor_last_name
 FROM
 	university.student
+JOIN
+	university.instructor ON student.advisor_id = instructor.instructor_id
 WHERE
 	student.advisor_id IS NOT NULL
 ;
